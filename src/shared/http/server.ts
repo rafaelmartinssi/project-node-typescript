1ffbfc3566598135cb570a0efc397afd87007b42
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-02-17 19:22:14 -0300

    Config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,16 +2,14 @@ import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server starded on port ${process.env.PORT}`)
-})
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Olá mundo' })
 })
