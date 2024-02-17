9bc0e7abd30c13411cdb364efc3e16dad58b6d6e
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-02-17 19:57:56 -0300

    Add errors middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,32 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
+import { AppError } from '@shared/errors/AppError'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(routes)
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      })
+    }
+    console.log(error)
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    })
+    next()
+  },
+)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server starded on port ${process.env.PORT}`)
