b53bf03a372ff1f30c9962eb564f8cf39bb9d49a
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-02-22 21:52:53 -0300

    Create migration AddRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,13 +1,18 @@
 import { DataSource } from 'typeorm'
+import { Role } from '@roles/entities/Role'
 import { CreateRolesTable1708384497492 } from './migrations/1708384497492-CreateRolesTable'
 import { CreateUsersTable1708647702032 } from './migrations/1708647702032-CreateUsersTable'
-import { Role } from '@roles/entities/Role'
+import { AddRoleIdToUsersTable1708648601182 } from './migrations/1708648601182-AddRoleIdToUsersTable'
 
 const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1708384497492, CreateUsersTable1708647702032],
+  migrations: [
+    CreateRolesTable1708384497492,
+    CreateUsersTable1708647702032,
+    AddRoleIdToUsersTable1708648601182,
+  ],
 })
 
 export { dataSource }
