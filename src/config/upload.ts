449775b2a53e1c091ed7b0ae330cb590294e5155
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-02-25 19:27:16 -0300

    Create multer config

---
+++ src/config/upload.ts
@@ -0,0 +1,23 @@
+import path from 'node:path'
+import crypto from 'node:crypto'
+import multer, { StorageEngine } from 'multer'
+
+type UploadConfig = {
+  directory: string
+  storage: StorageEngine
+}
+
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads')
+
+export default {
+  directory: uploadFolder,
+  storage: multer.diskStorage({
+    destination: uploadFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const filename = `${fileHash}_${file.originalname}`
+
+      callback(null, filename)
+    },
+  }),
+} as UploadConfig
