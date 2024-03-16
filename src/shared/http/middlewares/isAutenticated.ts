f3c5b4645670e1d26a4f2f81f2fa1b478daf06d9
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-03-16 17:20:25 -0300

    Create AccessAndRefreshTokenUseCase

--- src/shared/http/middlewares/isAutenticated.ts
+++ src/shared/http/middlewares/isAutenticated.ts
@@ -1,4 +1,3 @@
-import { AppError } from '@shared/errors/AppError'
 import { NextFunction, Request, Response } from 'express'
 import { JwtPayload, Secret, verify } from 'jsonwebtoken'
 import jwtConfig from '@config/auth'
@@ -10,10 +9,21 @@ export const isAutenticated = (
 ) => {
   const authHeader = request.headers.authorization
   if (!authHeader) {
-    throw new AppError('Failed to verify acess token', 401)
+    return response.status(401).json({
+      error: true,
+      code: 'token.invalid',
+      message: 'Access token not present',
+    })
   }
 
   const token = authHeader.replace('Bearer ', '')
+  if (!token) {
+    return response.status(401).json({
+      error: true,
+      code: 'token.invalid',
+      message: 'Access token not present',
+    })
+  }
 
   try {
     const decodedToken = verify(token, jwtConfig.jwt.secret as Secret)
@@ -21,6 +31,10 @@ export const isAutenticated = (
     request.user = { id: sub }
     return next()
   } catch {
-    throw new AppError('Invalid authentication token', 401)
+    return response.status(401).json({
+      error: true,
+      code: 'token.expired',
+      message: 'Access token not present',
+    })
   }
 }
